a596fe6cf8202d06996d81d786ff41d40abb7b9a
cyber-sapoperro <reclusive_coeditor@proton.me>
2026-05-04 21:03:09 -0500

    feat(tui): add input events feature

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+// use std::default;
+
+use ratatui::widgets::Borders;
 use time::OffsetDateTime;
 
 use crossterm::event;
@@ -9,24 +12,45 @@ use crossterm::event::KeyEventKind;
 use ratatui::DefaultTerminal;
 use ratatui::Frame;
 use ratatui::buffer::Buffer;
+use ratatui::layout::Constraint;
+use ratatui::layout::Direction;
+use ratatui::layout::Layout;
 use ratatui::layout::Rect;
 use ratatui::style::Modifier;
 use ratatui::style::Style;
 use ratatui::widgets::Block;
-use ratatui::widgets::Widget;
 use ratatui::widgets::Padding;
+use ratatui::widgets::Paragraph;
+use ratatui::widgets::Row;
+use ratatui::widgets::Table;
+use ratatui::widgets::Widget;
 use ratatui::widgets::calendar::CalendarEventStore;
 use ratatui::widgets::calendar::Monthly;
+use ratatui::widgets::Cell;
 
-use ratatui::layout::Layout;
-use ratatui::layout::Constraint;
-use ratatui::layout::Direction;
-
-use ratatui::widgets::Paragraph;
+#[derive(Debug)]
+struct Entry {
+    description: String,
+    hour: String,
+}
 
 #[derive(Debug, Default)]
 pub struct App {
     exit: bool,
+    entries: Vec<Entry>,
+
+    // input state
+    input_mode: InputMode,
+    input_desc: String,
+    input_hour: String,
+}
+
+#[derive(Debug, Default)]
+enum InputMode {
+    #[default]
+    Normal,
+    EditingDescription,
+    EditingHour,
 }
 
 impl App {
@@ -52,11 +76,67 @@ impl App {
         Ok(())
     }
 
-    fn handle_key_event(&mut self, key_event: KeyEvent) {
-        match key_event.code {
-            KeyCode::Char('q') => self.exit(),
-            _ => {}
+    fn handle_key_event(&mut self, key: KeyEvent) {
+        match self.input_mode {
+            InputMode::Normal =>
+                match key.code {
+                    KeyCode::Char('q') => self.exit(),
+                    KeyCode::Char('a') => {
+                        self.input_mode = InputMode::EditingDescription;
+                    }
+                    _ => {}
+                },
+
+            InputMode::EditingDescription => match key.code {
+                KeyCode::Enter => {
+                    self.input_mode = InputMode::EditingHour;
+                }
+                KeyCode::Char(c) => {
+                    self.input_desc.push(c);
+                }
+                KeyCode::Backspace => {
+                    self.input_desc.pop();
+                }
+                KeyCode::Esc => {
+                    self.cancel_input();
+                }
+                _ => {}
+            },
+
+            InputMode::EditingHour => match key.code {
+                KeyCode::Enter => {
+                    self.submit_event();
+                }
+                KeyCode::Char(c) if c.is_ascii_digit() => {
+                    self.input_hour.push(c);
+                }
+                KeyCode::Backspace => {
+                    self.input_hour.pop();
+                }
+                KeyCode::Esc => {
+                    self.cancel_input();
+                }
+                _ => {}
+            },
         }
+    }
+
+    fn submit_event(&mut self){
+        self.entries.push(Entry {
+            description: self.input_desc.clone(),
+            hour: self.input_hour.clone(),
+        });
+        self.reset_input();
+    }
+
+    fn cancel_input(&mut self) {
+        self.reset_input();
+    }
+
+    fn reset_input(&mut self) {
+        self.input_desc.clear();
+        self.input_hour.clear();
+        self.input_mode = InputMode::Normal;
     }
 
     fn exit(&mut self) {
@@ -71,6 +151,7 @@ impl Widget for &App {
             .constraints([
                 Constraint::Length(8),
                 Constraint::Min(0),
+                Constraint::Length(3),
             ])
             .split(area);
 
@@ -83,10 +164,44 @@ impl Widget for &App {
             .show_weekdays_header(Modifier::ITALIC)
             .render(chunks[0], buf);
 
-        Paragraph::new("Hello from ratatui!\nPress 'q' to quit.")
-            .block(Block::default().title("Info").borders(ratatui::widgets::Borders::ALL))
+        let mut rows: Vec<Row> = Vec::new();
+        for e in &self.entries {
+            rows.push(Row::new(vec![
+                Cell::from(e.hour.as_str()),
+                Cell::from(e.description.as_str()),
+            ]));
+        }
+
+        let widths = [
+            Constraint::Length(5),
+            Constraint::Length(5),
+        ];
+
+        Table::new(rows, widths)
+            .column_spacing(1)
+            .header(
+                Row::new(vec!["Hour", "Desc"])
+                .style(Style::new().bold())
+                .bottom_margin(1),
+                )
+            .block(Block::new().title("Today").borders(ratatui::widgets::Borders::ALL))
+            .highlight_symbol(">>")
             .render(chunks[1], buf);
 
+        let input = match self.input_mode {
+            InputMode::EditingDescription => {
+                Paragraph::new(format!("Desc: {}", self.input_desc))
+            }
+            InputMode::EditingHour => {
+                Paragraph::new(format!("Hour: {}", self.input_hour))
+            }
+            InputMode::Normal => {
+                Paragraph::new("Press 'a' to add events")
+            }
+        };
+
+        input.block(Block::default().title("New Event").borders(Borders::ALL))
+            .render(chunks[2], buf);
     }
 }
 
